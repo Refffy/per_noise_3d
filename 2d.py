83a5eb17dce29168623af64305adcb25d22ed427
Refffy <53311886+Refffy@users.noreply.github.com>
2020-08-03 21:24:52 +0600

    Update 2d.py

--- 2d.py
+++ 2d.py
@@ -2,10 +2,11 @@ import math
 from PIL import Image
 from noise import perlin, noise_octaves
 
-xy = (256, 256)
+xy = (800, 800)
 
 def gen_image(name, size, func, *args):
-    kx, ky, kz, kk = 0.01, 0.01, 0, 0x1000
+    #kx, ky, kz, kk = 0.01, 0.01, 0, 0x1000
+    kx, ky, kz, kk = 0.02, 0.02, 0.0, 0x3600
     image = Image.new('I', size)
     for j in range(size[1]):
         for i in range(size[0]):
@@ -13,5 +14,6 @@ def gen_image(name, size, func, *args):
             image.putpixel((i, j), math.floor(p*kk))
     image.save(name)
 
-gen_image("no_octaves.png", xy, perlin)
-gen_image("octaves.png", xy, noise_octaves, 5, 0.5)
+if __name__ == '__main__':
+    gen_image("no_octaves.png", xy, perlin)
+    gen_image("octaves.png", xy, noise_octaves, 1, 0.5)
